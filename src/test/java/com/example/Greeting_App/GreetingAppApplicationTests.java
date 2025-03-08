7fc791a41d31964758a8b1828aaaab9341d128c5
prince356 <princechaurasia014@gmail.com>
2025-03-08 14:37:54 +0530

    Initial commit - project setup

---
+++ src/test/java/com/example/Greeting_App/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Greeting_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
